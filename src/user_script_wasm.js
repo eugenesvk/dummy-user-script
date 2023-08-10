0bf53c168879d417a558f8392b0e2cce52a67511
eugenesvk <eugenesvk@users.noreply.github.com>
2023-08-10 13:14:05 +0700

    patch wasm-pack glue file to load wasm.js

--- src/user_script_wasm.js
+++ src/user_script_wasm.js
@@ -1,6 +1,5 @@
-import * as wasm from "./user_script_wasm_bg.wasm";
-import { __wbg_set_wasm } from "./user_script_wasm_bg.js";
+import * as wasm from "../pkg/user_script_wasm_bg.wasm.js";
+import { __wbg_set_wasm } from "../pkg/user_script_wasm_bg.js";
 __wbg_set_wasm(wasm);
-export * from "./user_script_wasm_bg.js";
 
 wasm.__wbindgen_start();
